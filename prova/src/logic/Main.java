16ed270979ced28885dbe075c67e0535df77d6eb
stefano <129210983+stefanousagithub@users.noreply.github.com>
2023-03-28 19:38:33 +0200

    First commit
    Add the example project

---
+++ prova/src/logic/Main.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class Main {
+    public static void main(String[] args) {
+    	int a = 3;
+        System.out.println(a); 
+    }
+}
---
+++ prova/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author stefano
+ *
+ */
+module prova {
+}
